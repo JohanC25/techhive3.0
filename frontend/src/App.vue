4ab2eee25ca145027921036bdc98f035359b92c3
David Trujillo <datrujillo2002@gmail.com>
2026-03-02 13:42:42 -0500

    feat chatbot

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <router-view />
+    <ChatBot />
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import ChatBot from '@/components/ChatBot.vue'
+</script>
